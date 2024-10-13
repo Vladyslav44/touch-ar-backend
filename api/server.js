9536bbb18197e3fdec2ad8b2dc63be938274e341
vladyslavsaienko <vladyslav.saienko@onicore.io>
2024-10-13 15:24:45 +0300

    integrate vercel.json (3)

--- api/server.js
+++ api/server.js
@@ -66,8 +66,8 @@ app.post('/send-message', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports = app;
